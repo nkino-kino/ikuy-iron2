9706bc0431d598cfa4ff1e540f5e4fc777deee0e
nkino-kino <80452548+nkino-kino@users.noreply.github.com>
2021-03-23 21:23:29 +0900

    Add files via upload

--- maketen_level0.js
+++ maketen_level0.js
@@ -1,5 +1,3 @@
-
-
 function maketen() {
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
@@ -11,12 +9,6 @@ function maketen() {
 }
 maketen();
  
-/*function createXMLHttpRequest() {
-    var XMLhttpObject = null;
-    XMLhttpObject = new XMLHttpRequest();
-    return XMLhttpObject;
-}
- */
 function createArray(csvData) {
 	var csvArray = [];
 	var tempArray = csvData.split("\n");
@@ -25,24 +17,6 @@ function createArray(csvData) {
 	}
 	var x = (csvArray[Math.floor(Math.random() * csvArray.length)][0]);
 
-	//document.write(csvArray[0]);
-	var mondai = document.getElementById("mondai");
-	mondai.innerHTML=(x);
-	
-	
-	/*
-	var x0 = (level0[Math.floor(Math.random() * level0.length)][0]);
-	var x1 = (level1[Math.floor(Math.random() * level1.length)][1]);
-	var x2 = (level2[Math.floor(Math.random() * level2.length)][2]);
-	var x3 = (level3[Math.floor(Math.random() * level3.length)][3]);
-		
-	var L0 = document.getElementById("L0");
-	L0.innerHTML=(x0);
-	var L1 = document.getElementById("L1");
-	L1.innerHTML=(x1);
-	var L0 = document.getElementById("L2");
-	L2.innerHTML=(x2);
-	var L1 = document.getElementById("L3");
-	L3.innerHTML=(x3);
-*/
+	document.write(x);
 }
+	
