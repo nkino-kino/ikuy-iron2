d1580c21734411ac66d36fd13ac692c04c011883
nkino-kino <80452548+nkino-kino@users.noreply.github.com>
2021-03-25 01:01:53 +0900

    Update 要素取得2.js

--- 要素取得2.js
+++ 要素取得2.js
@@ -6,7 +6,7 @@ var y = parseInt(a);
 var word1 = document.getElementById("word1");
 	word1.innerHTML=("英単語:  "+rec1[y]);
 var tr1 = document.getElementById("tr1");
-	tr1.innerHTML=("発音:  "+rec2[y]);
+	tr1.innerHTML=("意味:  "+rec2[y]);
 var pron1 = document.getElementById("pron1");
-	pron1.innerHTML=("意味:  "+rec3[y]);
+	pron1.innerHTML=("発音:  "+rec3[y]);
 }
