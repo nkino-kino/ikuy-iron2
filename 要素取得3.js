ed44da6382f1103830b2e1c623e873f21bfd3cef
nkino-kino <80452548+nkino-kino@users.noreply.github.com>
2021-03-25 15:53:09 +0900

    Add files via upload

---
+++ 要素取得3.js
@@ -0,0 +1,22 @@
+var y = 0;
+function hyouji2(){
+	if (y< rec.length){
+var word1 = document.getElementById("word1");
+	word1.innerHTML=("英単語:  "+rec1[y]);
+var tr1 = document.getElementById("tr1");
+	tr1.innerHTML=("発音:  "+rec2[y]);
+var pron1 = document.getElementById("pron1");
+	pron1.innerHTML=("意味:  "+rec3[y]);
+	y++;
+}
+	else if (y==rec.length){
+word1.innerHTML=("back to the first");
+y=0;
+}
+	else{
+		y=0;
+	word1.innerHTML=("英単語:  "+rec1[y]);
+	tr1.innerHTML=("発音:  "+rec2[y]);
+	pron1.innerHTML=("意味:  "+rec3[y]);
+	}
+	}
