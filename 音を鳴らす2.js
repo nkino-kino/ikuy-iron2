bf5d019bcd88a44a7c3f08a346b8864210866773
nkino-kino <nkinoshi@nibb.ac.jp>
2021-04-18 11:48:07 +0900

    Update 音を鳴らす2.js

--- 音を鳴らす2.js
+++ 音を鳴らす2.js
@@ -2,7 +2,7 @@ function sound()
 {
 	// 対象となるID名
 	//var id = 'sound-file' ;
-	var id = new Howl({ src: ['.Countdown05-1.mp3'] })
+	var id = new Howl({ src: ['Countdown05-1.mp3'] })
 	sound.play()
 
 	// 初回以外だったら音声ファイルを巻き戻す
