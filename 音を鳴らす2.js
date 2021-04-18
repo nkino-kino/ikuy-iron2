16c2461e509a9b04381e4fbe24f3b2d42f4efdee
nkino-kino <nkinoshi@nibb.ac.jp>
2021-04-18 10:39:38 +0900

    otowonarasu

---
+++ 音を鳴らす2.js
@@ -0,0 +1,16 @@
+function sound()
+{
+	// 対象となるID名
+	//var id = 'sound-file' ;
+	var id = new Howl({ src: ['.Countdown05-1.mp3'] })
+	sound.play()
+
+	// 初回以外だったら音声ファイルを巻き戻す
+	if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+	{
+		document.getElementById( id ).currentTime = 0;
+	}
+
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( id ).play() ;
+}
