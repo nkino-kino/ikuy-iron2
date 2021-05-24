4ea50c0adcb01f67056fca5c17df23c1406c6943
nkino-kino <nkinoshi@nibb.ac.jp>
2021-05-24 22:57:29 +0900

    PC2

--- TOEIC_wordsPC2.js
+++ TOEIC_wordsPC2.js
@@ -31,39 +31,3 @@ function createArray(csvData) {
 	document.getElementById("TOEIC").innerHTML=(csvArray[x][1]);
 }
 
-
-
-		
-//表示-------------	
-	
-/*	
-	for(var n=0; n<1253; n++){
-		word_num.push(csvArray[n][0]);
-		TOEIC.push(csvArray[n][1]);
-		definition.push(csvArray[n][2]);
-	}
-	var test1=document.getElementById("test1");
-		test1.innerHTML=definition[20];
-	var test2 = document.getElementById("test2");
-		test2.innerHTML=TOEIC[10];
-	
-	}
-	if (k<TOEIC.length){
-		var TOEICword = document.getElementById("TOEIC");
-		TOEICword.innerHTML=(word_num[k]+":  "+TOEIC[k]);
-		var definition2 = document.getElementById("definition");
-		definition2.innerHTML=(definition[k]);
-		k=k+1;
-	}
-	else if (k==TOEIC.length){
-		TOEIC = document.getElementById("TOEIC");
-		TOEIC.innerHTML=("back to the first");
-		k=0;
-	}
-	else{
-		k=0;
-		TOEIC.innerHTML=(word_num[k]+":  "+TOEIC[k]);
-}
-}
-
-*/
