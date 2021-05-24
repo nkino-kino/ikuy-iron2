1e4cb0ec148d6be93fbc48ac2254d8d04e5dcd41
nkino-kino <nkinoshi@nibb.ac.jp>
2021-05-24 22:23:17 +0900

    anaake

--- TOEIC_wordsPC2.js
+++ TOEIC_wordsPC2.js
@@ -1,8 +1,3 @@
-var k = 0;
-var csvArray = [];
-var word_num=[];
-var TOEIC=[];
-var definition=[];
 function getword() {
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
@@ -14,33 +9,43 @@ function getword() {
 }
 getword();
 
-/*function createXMLHttpRequest() {
+function createXMLHttpRequest() {
     var XMLhttpObject = null;
     XMLhttpObject = new XMLHttpRequest();
     return XMLhttpObject;
 }
- */
+
 function createArray(csvData) {
+	var csvArray = [];
 	var tempArray = csvData.split("\n");
     for(var i = 0; i<tempArray.length;i++){
 	csvArray[i] = tempArray[i].split(",");
 	}
 		
+	/*var min = 1;
+	var max = 1253;
+	var a = Math.floor(Math.random() * (max + 1 - min)) + min;
+	var x = parseInt(a);
+*/		}
+	document.getElementById("TOEIC").innerHTML=(csvArray[10][1])
+	document.getElementById("definition").innerHTML=(csvArray[10][2])
+
+
 
 		
 //表示-------------	
 	
-	
+/*	
 	for(var n=0; n<1253; n++){
 		word_num.push(csvArray[n][0]);
 		TOEIC.push(csvArray[n][1]);
 		definition.push(csvArray[n][2]);
-/*	}
+	}
 	var test1=document.getElementById("test1");
 		test1.innerHTML=definition[20];
 	var test2 = document.getElementById("test2");
 		test2.innerHTML=TOEIC[10];
-*/	
+	
 	}
 	if (k<TOEIC.length){
 		var TOEICword = document.getElementById("TOEIC");
@@ -60,24 +65,4 @@ function createArray(csvData) {
 }
 }
 
-//戻るfunctionーーーーーーーーーーーーーーー
-
-function back(){
-	var TOEICword = document.getElementById("TOEIC");
-		TOEICword.innerHTML=(word_num[k-1]+":  "+TOEIC[k-1]);
-		var definition2 = document.getElementById("definition");
-		definition2.innerHTML=(definition[k-1]);
-		k=k-1;
-}
-
-//番号入力るfunctionーーーーーーーーーーーーーーー
-
-function inputnum(){
-	var text01 = document.getElementById('wordnum');
-    k = parseInt(text01.value);
-		var TOEICword = document.getElementById("TOEIC");
-		TOEICword.innerHTML=(word_num[k-1]+":  "+TOEIC[k-1]);
-		var definition2 = document.getElementById("definition");
-		definition2.innerHTML=(definition[k-1]);
-}
-
+*/
